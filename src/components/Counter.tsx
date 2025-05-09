c4af92602a25fe11d8f414f93c67934ee5664973
Pdro Rivera <priveral@uni.pe>
2025-05-09 12:31:22 -0500

    use global context in counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,23 +1,49 @@
-import { useState } from 'react'
+import { useState, useContext, createContext } from 'react'
+
+type ContextType = {
+  count: number
+  addCount: () => void
+}
+
+const CountContext = createContext<ContextType | null>(null)
 
 export function Counter() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0)
+  const addCount = () => setCount(prev => prev + 1)
+  const contextValue = {
+    count,
+    addCount,
+  }
 
   return (
-    <div className="card">
-      <div className="card-header">
-        <h2>COUNTER</h2>
+    <CountContext value={contextValue}>
+      <div className="card">
+        <div className="card-header">
+          <h2>COUNTER</h2>
+        </div>
+        <div className="card-body">
+          <CountButton />
+        </div>
       </div>
-      <div className="card-body">
-        <button
-          type="button"
-          className="btn btn-outline-dark mt-2 mb-3"
-          onClick={() => setCount(count => count + 1)}
-        >
-          Count is: {count}
-        </button>
-        <p className="card-text fs-7">This is a counter button!</p>
-      </div>
-    </div>
+    </CountContext>
+  )
+}
+
+function CountButton() {
+  const context = useContext(CountContext)
+  if (!context) throw new Error('CountButton must be used within a Counter')
+  const { count, addCount } = context
+
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-outline-dark mt-2 mb-3"
+        onClick={addCount}
+      >
+        Count is: {count}
+      </button>
+      <p className="card-text fs-7">This is a counter button!</p>
+    </>
   )
 }
